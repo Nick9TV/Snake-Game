42330560cb46b2bd1254860572dd913be0cd062e
Mikołaj Hoffmann <e.m4il@op.pl>
2025-02-20 12:11:18 +0100

    Added 3 main languages

---
+++ Python/main.py
@@ -0,0 +1,36 @@
+import pygame
+import time
+import random
+
+sanake_speed = 10
+
+window_x = 720
+window_y = 720
+
+black = pygame.Color(0,0,0)
+white = pygame.Color(255,255,255)
+red = pygame.Color(255,0,0)
+green = pygame.Color(0,255,0)
+blue = pygame.Color(0,0,255)
+
+pygame.init()
+pygame.display.set_caption("Gra wąż")
+game_window = pygame.display.set_mode((window_x, window_y))
+
+fps = pygame.time.Clock()
+
+snake_position = [100, 50]
+snake_body = [
+    [100,50],
+    [90,50],
+    [80,50],
+    [70,50]
+]
+
+fruit_position = [random.randrange(1, (window_x//10)) * 10,
+                  random.randrange(1, (window_x//10)) * 10]
+
+fruit_spawn = True
+
+direction = 'RIGHT'
+change_to = direction
